fc1b7d3ff008ed2d8cd09a37d85d4dcbd5ac2256
Tyzashi <ty.reese@yahoo.com>
2018-06-22 01:13:02 -0400

    Finalizing

---
+++ IMD274 - Tyler Whittemore-Reese/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public GameObject winText;
+    public float resetDelay;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != null)
+        { 
+            Destroy(gameObject);
+
+        }
+
+    }
+
+    public void Win()
+    {
+        winText.SetActive(true);
+        Invoke("Reset", resetDelay);
+    }
+
+    public void Reset()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
